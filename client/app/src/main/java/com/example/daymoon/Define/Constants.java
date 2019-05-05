fffa5a5e913a58cc86528907b805e629724ffe3e
deskmel <deskmel@sjtu.edu.cn>
2019-05-05 08:54:09 +0800

    常量定义

---
+++ client/app/src/main/java/com/example/daymoon/Define/Constants.java
@@ -0,0 +1,14 @@
+package com.example.daymoon.Define;
+
+public class Constants {
+    /**
+     * @NORMAL 正常状态
+     */
+    public static final int NORMAL = 0;
+
+    /**
+     * @ERROR 错误状态
+     */
+    public static final int ERROR = 1;
+    //
+}
